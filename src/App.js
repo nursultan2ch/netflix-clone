a8e8a6613844c4ab46cebf471773cec7797dc5f7
nursultan2ch <nsoodonbekov65@gmail.com>
2023-01-29 19:00:31 +0600

    netflix-clone

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import { AuthContextProvider } from "./pages/context/AuthContext";
+import Login from "./pages/Login"
+import Account from "./pages/Account"
+import Signup from "./pages/Signup"
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/account" element={<ProtectedRoute><Account /> </ProtectedRoute>}/>
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
